73929bcfc4ed4d7609ca5d264988cca765683948
Jan <janhajk@gmail.com>
2017-12-06 14:42:00 +0000

    cleanup

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
 ========================================-->
 <?php if ( is_active_sidebar( 'footer-widget-area' ) ) { ?>
 <footer class="footer-sidebar" role="contentinfo">
-        <div class="background-overlay">
-                <div class="container">
-                        <div class="row padding-top-60 padding-bottom-60">
-                                <?php  dynamic_sidebar( 'footer-widget-area' ); } ?>
-                        </div>
-                </div>
+    <div class="background-overlay">
+        <div class="container">
+            <div class="row padding-top-60 padding-bottom-60">
+                <?php  dynamic_sidebar( 'footer-widget-area' ); } ?>
+            </div>
         </div>
+    </div>
 </footer>
 
 <div class="clearfix"></div>
@@ -22,51 +22,30 @@
     <div class="container">
         <div class="row padding-top-20 padding-bottom-10 ">
             <div class="col-md-6 text-left">
-                                <?php
-                                        $copyright_content= get_theme_mod('copyright_content',__('Your Copyright Text','specia'));
-                                        $hide_show_copyright= get_theme_mod('hide_show_copyright','on');
-
-                                ?>
-                                <?php if($hide_show_copyright == 'on') : ?>
-                                        <p><?php echo $copyright_content; ?>
-                                <?php endif; ?>
-                </p>
+                <?php
+                   $copyright_content= get_theme_mod('copyright_content',__('Your Copyright Text','specia'));
+                   $hide_show_copyright= get_theme_mod('hide_show_copyright','on');
+                ?>
+                    <?php if($hide_show_copyright == 'on') : ?>
+                    <p>
+                        <?php echo $copyright_content; ?>
+                        <?php endif; ?>
+                    </p>
             </div>
 
             <div class="col-md-6">
-                                <?php
-                                        $hide_show_payment = get_theme_mod('hide_show_payment','on');
-                                        $icon_one= get_theme_mod('icon_one','');
-                                        $icon_two= get_theme_mod('icon_two','');
-                                        $icon_three= get_theme_mod('icon_three','');
-                                        $icon_four= get_theme_mod('icon_four','');
-                                        $icon_five= get_theme_mod('icon_five','');
+                <?php
+                         $hide_show_payment = get_theme_mod('hide_show_payment','on');
+                         $icon_one= get_theme_mod('icon_one','');
+                         $icon_two= get_theme_mod('icon_two','');
+                         $icon_three= get_theme_mod('icon_three','');
+                         $icon_four= get_theme_mod('icon_four','');
+                         $icon_five= get_theme_mod('icon_five','');
+                 ?>
 
-                                ?>
-
-                                <?php if($hide_show_payment == 'on') { ?>
-                                        <ul class="payment-icon">
-                                                <?php if($icon_one) { ?>
-                                                        <li><a href="<?php echo esc_url($icon_one); ?>"><i class="fa fa-cc-paypal"></i></a></li>
-                                                <?php } ?>
-
-                                                <?php if($icon_two) { ?>
-                                                        <li><a href="<?php echo esc_url($icon_two); ?>"><i class="fa fa-cc-visa"></i></a></li>
-                                                <?php } ?>
-
-                                                <?php if($icon_three) { ?>
-                                                        <li><a href="<?php echo esc_url($icon_three); ?>"><i class="fa fa-cc-mastercard"></i></a></li>
-                                                <?php } ?>
-
-                                                <?php if($icon_four) { ?>
-                                                        <li><a href="<?php echo esc_url($icon_four); ?>"><i class="fa fa-cc-amex"></i></a></li>
-                                                <?php } ?>
-
-                                                <?php if($icon_five) { ?>
-                                                        <li><a href="<?php echo esc_url($icon_five); ?>"><i class="fa fa-cc-stripe"></i></a></li>
-                                                <?php } ?>
-                                        </ul>
-                                <?php } ?>
+                    <ul class="payment-icon">
+                        <li><a href="<?php echo esc_url($icon_one); ?>"><i class="fa fa-cc-paypal"></i></a></li>
+                    </ul>
             </div>
         </div>
     </div>
@@ -80,5 +59,5 @@
 <?php wp_footer(); ?>
 <?php the_field('footer_script'); ?>
 </body>
-</html>
 
+</html>
--- templates/template-homepage-one.php
+++ templates/template-homepage-one.php
@@ -6,8 +6,8 @@ get_header();
 
 	get_template_part('sections/specia','call-action');
 	get_template_part('sections/specia','slider');
-	get_template_part('sections/specia','service');	
-	get_template_part('sections/specia','portfolio');		
+	get_template_part('sections/specia','service');
+	get_template_part('sections/specia','portfolio');
 	get_template_part('sections/specia','blog');
-	
+
 get_footer();

bf8c7754446b24fb08b761b5a687082f3690f7b5
janhajk <janhajk@gmail.com>
2019-02-09 12:10:10 +0000

    add parent styles

--- functions.php
+++ functions.php
@@ -9,9 +9,8 @@
 add_action( 'wp_enqueue_scripts', 'electro_shopofthings_enqueue_styles' );
 function electro_shopofthings_enqueue_styles() {
  
-    $parent_style = 'electro'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
- 
-    //wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    $parent_style = 'electro';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'electro-shopofthings',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),

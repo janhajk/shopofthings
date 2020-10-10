4d4544c5fa8477359ee33084d1d71ad99810b3a8
Jan <janhajk@gmail.com>
2020-10-10 16:52:30 +0000

    facebook pixel

--- functions.php
+++ functions.php
@@ -21,8 +21,8 @@ function createAdsenseBlogResponsive() {
     // // This creates the Reader object, which should be reused across
     // // lookups.
     // $reader = new Reader('/var/www/vhosts/jan/shopofthings/wordpress/wp-content/geoip/GeoLite2-Country_20190205/GeoLite2-Country.mmdb');
-    
-    
+
+
     // if (!isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
     //     $IP = $_SERVER['REMOTE_ADDR'];
     // }
@@ -73,7 +73,37 @@ add_filter( 'jetpack_just_in_time_msgs', '__return_false' );
 add_filter( 'woocommerce_min_password_strength', 'wpglorify_woocommerce_password_filter', 10 );
 function wpglorify_woocommerce_password_filter() {
       return 1;
-} 
+}
+
+add_filter( 'allow_subdirectory_install', create_function( '', 'return true;' ));
+
+
+
+
+/***** Adding Facebook Pixel *****/
+add_action('wp_head', 'oiw_add_fbpixel');
+
+function oiw_add_fbpixel() {
+?>
+<!-- Facebook Pixel Code -->
+      <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '2735434730048399');
+      fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=2735434730048399&ev=PageView&noscript=1"
+      /></noscript>
+<!-- End Facebook Pixel Code -->
+<?php
+}
 
 
 

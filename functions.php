f76fd5eb9f8a1d5803a96e36ddea77f735296dc5
Jan <janhajk@gmail.com>
2020-10-26 12:03:45 +0000

    linkedin tag

--- functions.php
+++ functions.php
@@ -106,5 +106,31 @@ function oiw_add_fbpixel() {
 }
 
 
+/***** Adding Facebook Pixel *****/
+add_action('wp_head', 'oiw_add_linkedin');
+
+function oiw_add_linkedin() {
+?>
+<script type="text/javascript">
+_linkedin_partner_id = "2806769";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+</script><script type="text/javascript">
+(function(){var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})();
+</script>
+<noscript>
+<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=2806769&fmt=gif" />
+</noscript>
+<?php
+}
+
+
+
+
+
 
 ?>

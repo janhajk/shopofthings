1f6e5db83c1c7c2d4db911a0bf7fd667daea15bc
Jan <janhajk@gmail.com>
2020-11-23 20:10:20 +0000

    remove archive from titles

--- functions.php
+++ functions.php
@@ -129,6 +129,26 @@ s.parentNode.insertBefore(b, s);})();
 }
 
 
+/**
+ * 
+ * Remove "Archive" from all page titles
+ * 
+ * 
+ * 
+ */
+add_filter( 'get_the_archive_title', function ($title) {
+ if ( is_category() ) {
+ $title = single_cat_title( '', false );
+ } elseif ( is_tag() ) {
+ $title = single_tag_title( '', false );
+ } elseif ( is_author() ) {
+ $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+ }
+ 
+ return $title;
+});
+
+
 
 
 
